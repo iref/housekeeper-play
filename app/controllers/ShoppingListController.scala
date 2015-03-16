2b318b1a8e0f59b0b6b1e0920e86910bc5773d4e
Jan Ferko <julyloov@gmail.com>
2015-03-16 15:59:08 +0100

    Add ShoppingLists#show route

--- app/controllers/ShoppingListController.scala
+++ app/controllers/ShoppingListController.scala
@@ -4,12 +4,17 @@ import models.ShoppingListRepository
 import play.api.Play.current
 import play.api.db.slick.Config.driver.simple._
 import play.api.db.slick._
-import play.api.mvc.Controller
+import play.api.mvc.{Action, Controller}
 
 class ShoppingListController(shoppingListRepository: ShoppingListRepository) extends Controller {
 
   def index = DBAction { implicit rs =>
     val shoppingLists = shoppingListRepository.all
     Ok(views.html.shoppingList.index(shoppingLists))
+  }
+
+  def show(id: Int) = DBAction { implicit rs =>
+    val shoppingListDetail = shoppingListRepository.find(id)
+    Ok(views.html.shoppingList.show(shoppingListDetail))
   }
 }
--- test/controllers/ShoppingListControllerSpec.scala
+++ test/controllers/ShoppingListControllerSpec.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import models.{ShoppingList, ShoppingListRepository}
+import models.{ShoppingListItem, ShoppingListDetail, ShoppingList, ShoppingListRepository}
 import org.specs2.mock.Mockito
 import play.api.db.DB
 import play.api.db.slick.Config.driver.simple._
@@ -9,11 +9,11 @@ import play.api.test.{FakeRequest, PlaySpecification, WithApplication}
 class ShoppingListControllerSpec extends PlaySpecification with Mockito {
 
   val shoppingListRepository = mock[ShoppingListRepository]
+  val controller = new ShoppingListController(shoppingListRepository)
 
   "ShoppingListController#index" should {
     "render shopping list template" in new WithApplication {
       // given
-      val controller = new ShoppingListController(shoppingListRepository)
       shoppingListRepository.all(any[Session]) returns List()
 
       // when
@@ -26,7 +26,6 @@ class ShoppingListControllerSpec extends PlaySpecification with Mockito {
 
     "render info message if there aren't any shopping lists" in new WithApplication {
       // given
-      val controller = new ShoppingListController(shoppingListRepository)
       shoppingListRepository.all(any[Session]) returns List()
 
       // when
@@ -40,7 +39,6 @@ class ShoppingListControllerSpec extends PlaySpecification with Mockito {
 
     "render all shopping lists" in new WithApplication {
       // given
-      val controller = new ShoppingListController(shoppingListRepository)
       val shoppingLists = List(
         ShoppingList("First list", "Newbie list", Some(1)),
         ShoppingList("Second list", "My awesome list", Some(2)))
@@ -55,6 +53,59 @@ class ShoppingListControllerSpec extends PlaySpecification with Mockito {
       contentAsString(result) must not contain("No shopping lists were created yet.")
       shoppingLists.foreach { sl =>
         contentAsString(result) must contain(sl.title)
+      }
+    }
+  }
+
+  "ShoppingList#show" should {
+
+    "render shopping list detail template" in new WithApplication {
+      // when
+      val result = controller.show(1)(FakeRequest())
+      shoppingListRepository.find(any[Int])(any[Session]) returns(None)
+
+      // then
+      status(result) must equalTo(OK)
+      contentType(result) must beSome("text/html")
+      contentAsString(result) must contain ("Shopping List Detail | Housekeeper")
+    }
+
+    "render message if shopping detail doesn't exists" in new WithApplication {
+      // given
+      shoppingListRepository.find(any[Int])(any[Session]) returns(None)
+
+      // when
+      val result = controller.show(1)(FakeRequest())
+
+      // then
+      status(result) must equalTo(OK)
+      contentType(result) must beSome("text/html")
+      contentAsString(result) must contain ("Shopping list was not found.")
+    }
+
+    "render shopping list detail" in new WithApplication {
+      // given
+      val shoppingListDetail = ShoppingListDetail(
+        ShoppingList("First list", "Newbie list", Some(1)),
+        List(
+          ShoppingListItem("Brewdog 5am Saint Red Ale", 1, None, Some(1), Some(1)),
+          ShoppingListItem("Macbook Air 13", 1, Some(1000.0), Some(1), Some(2))
+        )
+      )
+      shoppingListRepository.find(org.mockito.Matchers.eq(1))(any[Session]) returns Some(shoppingListDetail)
+
+      // when
+      val result = controller.show(1)(FakeRequest())
+
+      // then
+      status(result) must equalTo(OK)
+      contentType(result) must beSome("text/html")
+      contentAsString(result) must contain(shoppingListDetail.shoppingList.title)
+      contentAsString(result) must contain(shoppingListDetail.shoppingList.description)
+      shoppingListDetail.items.foreach { item =>
+        contentAsString(result) must contain(item.name)
+        contentAsString(result) must contain(item.quantity.toString)
+        contentAsString(result) must contain(item.priceForOne.map(_.toString).getOrElse("-"))
       }
     }
   }
